ce4fd8332afe197186b326475f09da8f58ee5445
Atabay <Atabaj.Arniyazov@p97.com>
2020-07-11 03:45:59 +0300

    customised the context menu for “Video Title” field;

--- src/app/video-item/video-item.service.ts
+++ src/app/video-item/video-item.service.ts
@@ -14,7 +14,7 @@ export class VideoItemService {
   constructor(private http: HttpClient) {
   }
 
-  public getData(): Promise<any> {
+  public getData(): any {
     const url1 = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk&maxResults=50&type=video&part=snippet&q=john';
     const url2 = 'assets/linkData.json'; //use this url2 if you got 403 from url1
 
